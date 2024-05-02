1f439b24deda1bccdf29544f63771a37ab9afdfe
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-05-02 10:55:06 -0700

    Q23 ts/js node js of 45 assignment
    Q23ts/js node js of 45 assignment

---
+++ q23.js
@@ -0,0 +1,26 @@
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+//.1
+var car = "subaru";
+console.log(car == "subaru");
+console.log(car == "city");
+//.2
+var num = 32;
+console.log(num === 32);
+console.log(num === "32");
+//3
+var person = "sonia";
+console.log(person == "sonia");
+console.log(person == "sadia");
+//4 equality check
+var fruit1 = "apple";
+var fruit2 = "strawberry";
+console.log(fruit1 !== fruit2);
+console.log(fruit1 === fruit2);
+//5Greater than check
+console.log(fruit1 < fruit2);
+console.log(fruit1 > fruit2);
